b74f1fc5cedcfa0d0166a423cd888cd9a6a332e4
jagadishck018 <jagadishck.018@gmail.com>
2025-03-24 12:12:42 +0530

    add dotenv for environment variable management and update JWT secret key handling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import "dotenv/config";
 
 const payload: jwt.JwtPayload = {
   iss: 'jagadishck.018@gmail.com',
@@ -6,22 +7,21 @@ const payload: jwt.JwtPayload = {
   
 }
 
-const scretKey = "helloWorld@123";
+const jwtSecretKey = process.env.SCERET_KEY || process.exit(1);
+console.log(jwtSecretKey);
 
-const token = jwt.sign(payload, scretKey, {
-  algorithm: 'HS256',
-  expiresIn: '7d',
-
-  
+const token = jwt.sign(payload, jwtSecretKey, {
+  algorithm: "HS256",
+  expiresIn: "7d",
 });
  
 console.log(token);
 
 const decoded = jwt.decode(token);
-console.log(decoded);
+console.log(decoded,"\n");
 
 try {
-  const verified = jwt.verify(token, scretKey);
+  const verified = jwt.verify(token, jwtSecretKey);
   console.log(verified);
 }
 catch (error) {
